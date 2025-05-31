efc7ede4aa4098945bb3109eaa39980a238e2c1a
Marcel46509 <szczyglowskimarcel@gmail.com>
2025-05-31 15:18:00 +0200

    Test

---
+++ assets/js/items.js
@@ -0,0 +1,41 @@
+fetch("../assets/json/products.json")
+.then(response => response.json())
+.then(items => {
+    const allContainer = document.getElementById("all-container")
+    const bestSellersContainer = document.getElementById("best-sellers-container")
+
+    items.forEach(element => {
+        const itemDiv = document.createElement("div")
+        itemDiv.className = "item"
+
+        itemDiv.innerHTML = `
+        <div class="item-image">
+            <a class="item-image" href="${element.link}" rel="nofollow"><img src="../assets/images/${element.image}"/></a>
+        </div>
+
+        <div class="item-description">
+            <h2>${element.name}</h2>
+            <b>${element.price}</b>
+        </div>
+        `
+
+        if (element.bestSeller && bestSellersContainer)
+        {
+            bestSellersContainer.appendChild(itemDiv)
+        }
+        else
+        {
+            const customContainer = document.getElementById(element.type + "-container")
+
+            if (customContainer)
+            {
+                customContainer.appendChild(itemDiv)
+            }
+            else if (allContainer)
+            {
+                allContainer.appendChild(itemDiv)
+            }
+        }
+    })
+})
+.catch(error => console.error("Failed to load items:", error))
---
+++ assets/js/navbar.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("../navbar.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("navbar-container").innerHTML = data
+
+            // Currencies
+
+            // List
+            fetch("../assets/json/currencies.json")
+            .then(response => response.json())
+            .then(currencies => {
+                const currenciesContainer = document.getElementById("currencies-container")
+
+                currencies.forEach(element => {
+                    const currencyA = document.createElement("a")
+                    currencyA.textContent = element
+                    currencyA.setAttribute("data-currency", element)
+
+                    currenciesContainer.appendChild(currencyA)
+            })
+
+            // Handlers
+            const currencyButton = document.getElementById("currency-button")
+            const currencyOptions = document.querySelectorAll(".currency-options a")
+
+            // Load saved currency on page load
+            const savedCurrency = localStorage.getItem("selectedCurrency")
+
+            if (!savedCurrency)
+            {
+                localStorage.setItem("selectedCurrency", "PLN")
+            }
+
+            if (savedCurrency) 
+            {
+                currencyButton.textContent = savedCurrency
+            }
+
+            // Save and Set Currency
+            currencyOptions.forEach(option => {
+                option.addEventListener("click", function(element)
+                {
+                    element.preventDefault()
+
+                    const selectedCurrency = this.getAttribute("data-currency")
+                    currencyButton.textContent = selectedCurrency
+
+                    localStorage.setItem("selectedCurrency", selectedCurrency)
+                })
+            })
+        })
+    })
+    .catch(error => console.error("Error loading navbar:", error))
+})
